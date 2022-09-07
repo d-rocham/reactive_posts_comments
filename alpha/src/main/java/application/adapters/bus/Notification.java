6102f3f9c1b27ae67f35d610fa0b6e9e439b1e69
d-rocham <d_rocham@outlook.com>
2022-09-06 19:44:11 -0500

    Implemented adapters

---
+++ alpha/src/main/java/application/adapters/bus/Notification.java
@@ -0,0 +1,47 @@
+package application.adapters.bus;
+
+import com.google.gson.Gson;
+
+import java.time.Instant;
+
+public class Notification {
+
+    private final String type;
+    private final String body;
+    private final Instant instant;
+
+    public Notification(String type, String body) {
+        this.type = type;
+        this.body = body;
+        this.instant = Instant.now();
+    }
+
+    // What is this constructor used for?
+    public Notification() {
+        this(null, null);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    public Notification deserialize(String serialization) {
+        return new Gson().fromJson(serialization, Notification.class);
+    }
+
+    public String serialize() {
+        return new Gson().toJson(this);
+    }
+
+    public static Notification from(String notification) {
+        return new Notification().deserialize(notification);
+    }
+}
