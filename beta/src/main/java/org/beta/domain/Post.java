09700bae41ed7c1148c6346472ad616485a3224d
d-rocham <d_rocham@outlook.com>
2022-09-07 16:16:32 -0500

    Started work on Beta microservice, added domain layer, installed dependencies

---
+++ beta/src/main/java/org/beta/domain/Post.java
@@ -0,0 +1,121 @@
+package org.beta.domain;
+
+import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
+import org.alpha.domain.entities.Comment;
+import org.alpha.domain.entities.Reaction;
+import org.alpha.domain.events.*;
+import org.alpha.domain.identifiers.CommentID;
+import org.alpha.domain.identifiers.PostID;
+import org.alpha.domain.identifiers.ReactionID;
+import org.alpha.domain.valueobjects.Author;
+import org.alpha.domain.valueobjects.Content;
+import org.alpha.domain.valueobjects.ReactionType;
+import org.alpha.domain.valueobjects.Title;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Post extends AggregateEvent<PostID> {
+
+    protected Author author;
+    protected Title title;
+
+    protected List<Comment> commentList;
+    protected List<Reaction> reactionList;
+
+    public Post(PostID entityId, Author author, Title title) {
+        super(entityId);
+        subscribe(new PostChange(this));
+        appendChange(new PostCreated(
+                title.value(),
+                author.value()
+        )).apply();
+    }
+
+    private Post(PostID postID) {
+        super(postID);
+        subscribe(new PostChange(this));
+    }
+
+    public static Post from(PostID id, List<DomainEvent> postEvents) {
+        Post post = new Post(id);
+        postEvents.forEach(post::applyEvent);
+
+        return post;
+    }
+
+    // Finders
+
+    public Optional<Comment> getCommentById(CommentID targetCommentId) {
+        return commentList.stream()
+                .filter(comment -> comment.identity().equals(targetCommentId))
+                .findFirst();
+    }
+
+    public Optional<Reaction> getReactionById(ReactionID targetReactionId) {
+        return reactionList.stream()
+                .filter(reaction -> reaction.identity().equals(targetReactionId))
+                .findFirst();
+    }
+
+
+    // Behaviours
+
+    public void addComment(CommentID commentID, Author author, Content content) {
+        Objects.requireNonNull(commentID);
+        Objects.requireNonNull(author);
+        Objects.requireNonNull(content);
+
+        // Create event
+        appendChange(
+                new CommentAdded(commentID.value(), author.value(), content.value())
+        ).apply();
+
+    }
+
+    public void addReaction(ReactionID reactionID, Author author, ReactionType reactionType) {
+        Objects.requireNonNull(reactionID);
+        Objects.requireNonNull(author);
+        Objects.requireNonNull(reactionType);
+
+        // Create event
+        appendChange(
+                new ReactionAdded(reactionID.value(), author.value(), reactionType.value())
+        ).apply();
+    }
+
+    public void editComment(CommentID commentID, Content content) {
+        Objects.requireNonNull(commentID);
+        Objects.requireNonNull(content);
+
+        // Create event
+        appendChange(
+                new CommentEdited(commentID.value(), content.value())
+        ).apply();
+    }
+
+    public void editReaction(ReactionID reactionID, ReactionType reactionType) {
+        Objects.requireNonNull(reactionID);
+        Objects.requireNonNull(reactionType);
+
+        appendChange(
+                new ReactionEdited(reactionID.value(), reactionType.value())
+        ).apply();
+    }
+
+    public void editPost(PostID postID, Title title) {
+        Objects.requireNonNull(postID);
+        Objects.requireNonNull(title);
+
+        appendChange(
+                new PostEdited(postID.value(), title.value())
+        ).apply();
+    }
+
+    public void modifyTitle(Title updatedTitle) {
+        this.title = Objects.requireNonNull(updatedTitle);
+    }
+
+}
---
+++ beta/src/main/java/org/beta/domain/commands/CreatePost.java
@@ -0,0 +1,38 @@
+package org.beta.domain.commands;
+
+import co.com.sofka.domain.generic.Command;
+import lombok.Setter;
+import lombok.ToString;
+
+// TODO: Check if annotations below are actually necessary
+@Setter
+@ToString
+public class CreatePost extends Command {
+
+    private String postID;
+    private String author;
+    private String title;
+
+    // What is the purpose of this empty constructor
+    public CreatePost() {
+
+    }
+
+    public CreatePost(String postID, String author, String title) {
+        this.postID = postID;
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
