68a09508a7af551399a35197d313e4c8b9849838
d-rocham <d_rocham@outlook.com>
2022-09-08 17:43:30 -0500

    Implemented QueueHandler from mentor's example, understood data flow

--- beta/src/main/java/org/beta/application/handlers/QueueHandler.java
+++ beta/src/main/java/org/beta/application/handlers/QueueHandler.java
@@ -1,4 +1,50 @@
 package org.beta.application.handlers;
 
-public class QueueHandler {
+import co.com.sofka.domain.generic.DomainEvent;
+import com.google.gson.Gson;
+import org.beta.application.adapters.bus.Notification;
+import org.beta.business.usecases.UpdateViewUseCase;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
+
+@Service
+public class QueueHandler implements Consumer<String> {
+
+    private final Gson gson = new Gson();
+    private final UpdateViewUseCase updateViewUseCase;
+
+    public QueueHandler(UpdateViewUseCase updateViewUseCase) {
+        this.updateViewUseCase = updateViewUseCase;
+    }
+
+    @Override
+    public void accept(String jsonView) {
+
+        // Deserialize the incoming message into a notification
+        Notification notification = gson.fromJson(jsonView, Notification.class);
+        // Below operation could possibly be avoided if the Domain layer is turned into its own module
+        // TODO: learn how to turn the domain layer into an installable module
+        String type = notification
+                .getType()
+                // Due to my own file structure, line below might trigger errors.
+                .replace("alpha", "beta");
+
+        try {
+            // Try to cast the Notification into a DomainEvent
+            //(super class from which all the domain events extend from).
+
+            // This implementation is scalable, because any future new
+            // domain event will fit as long as it extends from DomainEvent
+            DomainEvent receivedEvent = (DomainEvent) gson.fromJson(
+                    notification.getBody(),
+                    Class.forName(type));
+
+            // Should the notification fit with the DomainEvent type, activate the updateViewUseCase
+            updateViewUseCase.accept(receivedEvent);
+        } catch (ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
+
+    }
 }
