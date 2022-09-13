ab12ad62ff79815925157d1c680724dc5d7622b4
d-rocham <d_rocham@outlook.com>
2022-09-12 20:51:36 -0500

    Barebones implementation of loginPrompt, which will be used once users are implemented. Implemented routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { PostViewComponent } from './views/post-view/post-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/homeView', pathMatch: 'full' },
+  { path: 'homeView', component: HomeViewComponent },
+  { path: 'postView/:postId', component: PostViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
