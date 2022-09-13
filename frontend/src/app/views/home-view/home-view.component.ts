4b3e4fd06b349f326562a00bb551ffedb639c532
d-rocham <d_rocham@outlook.com>
2022-09-13 16:46:08 -0500

    Refactored views implemented data flow methods & injected services

--- frontend/src/app/views/home-view/home-view.component.ts
+++ frontend/src/app/views/home-view/home-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { POSTS } from 'src/app/mock_data/mock-posts';
+import { WebSocketSubject } from 'rxjs/webSocket';
+import { Post } from 'src/app/models/post';
+import { WebRequestsService } from 'src/app/services/web-requests.service';
+import { WebSocketService } from 'src/app/services/web-socket.service';
 
 @Component({
   selector: 'app-home-view',
@@ -7,9 +10,37 @@ import { POSTS } from 'src/app/mock_data/mock-posts';
   styleUrls: ['./home-view.component.css'],
 })
 export class HomeViewComponent implements OnInit {
-  posts = POSTS;
+  viewSocket!: WebSocketSubject<Post>;
 
-  constructor() {}
+  sessionPosts: Post[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private socket: WebSocketService,
+    private requestService: WebRequestsService
+  ) {}
+
+  // TODO: Add create post functionality
+
+  ngOnInit(): void {
+    this.getPosts();
+    this.connectToSocket();
+  }
+
+  addPost(post: Post) {
+    this.sessionPosts.unshift(post);
+  }
+
+  connectToSocket() {
+    this.viewSocket = this.socket.mainViewSocket();
+    this.viewSocket.subscribe((post) => {
+      this.addPost(post);
+    });
+  }
+
+  getPosts() {
+    this.requestService.getAllPosts().subscribe((incomingPosts) => {
+      this.sessionPosts = incomingPosts;
+      console.log(this.sessionPosts);
+    });
+  }
 }
--- frontend/src/app/views/post-view/post-view.component.ts
+++ frontend/src/app/views/post-view/post-view.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { Comment } from 'src/app/models/comment';
+import { ActivatedRoute } from '@angular/router';
+import { WebSocketService } from 'src/app/services/web-socket.service';
+import { WebRequestsService } from 'src/app/services/web-requests.service';
 
 @Component({
   selector: 'app-post-view',
   templateUrl: './post-view.component.html',
-  styleUrls: ['./post-view.component.css']
+  styleUrls: ['./post-view.component.css'],
 })
 export class PostViewComponent implements OnInit {
+  post!: Post;
+  viewSocket?: WebSocketSubject<Comment>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private requestService: WebRequestsService,
+    private location: Location,
+    private socketService: WebSocketService
+  ) {}
 
   ngOnInit(): void {
+    this.getPost();
   }
 
+  getPost() {
+    const targetId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if (targetId) {
+      this.requestService.getPostById(targetId).subscribe((foundPost) => {
+        this.post = foundPost;
+        this.connectToOwnSocket(
+          this.post ? this.post.aggregateId : 'mainSpace'
+        );
+      });
+    }
+  }
+
+  connectToOwnSocket(path: string) {
+    this.viewSocket = this.socketService.postViewSocket(path);
+    this.viewSocket.subscribe((incomingComment) =>
+      this.addComment(incomingComment)
+    );
+  }
+
+  addComment(newComment: Comment) {
+    this.post?.comments.unshift(newComment);
+  }
+
+  // TODO: create comment functionality
 }
