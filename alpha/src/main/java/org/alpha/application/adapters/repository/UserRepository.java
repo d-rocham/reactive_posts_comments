ed2037857bf5594346bffe603ef41713b51daa70
d-rocham <d_rocham@outlook.com>
2022-09-14 16:06:47 -0500

    Added repository, model for user authentication

---
+++ alpha/src/main/java/org/alpha/application/adapters/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.alpha.application.adapters.repository;
+
+import org.alpha.application.generic.models.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByUsername(String username);
+}
