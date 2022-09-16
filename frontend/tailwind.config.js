2a3b520f87712f6f2b8f5da4c6c84c87588611e4
d-rocham <d_rocham@outlook.com>
2022-09-16 08:12:06 -0500

    Styling touches

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-	"./src/**/*.{html,ts}",
-],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      dropShadow: {
+        "md-top": "0 -4px 3px rgb(0 0 0 / 0.07)",
+        "lg-inner": "inset 0 4px 5px 0 rgb(0 0 0 / 0.05)",
+      },
+    },
   },
-  plugins: [
-	require("daisyui"),
-	require('@tailwindcss/forms')
-],
-}
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
+};
